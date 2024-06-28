29506e34d3e15170370a5b6035bfcab543942190
Arun <arun007@pm.me>
2024-06-28 10:22:30 +0530

    Update build.sh

--- packages/perl/build.sh
+++ packages/perl/build.sh
@@ -10,12 +10,12 @@ TERMUX_PKG_MAINTAINER="@termux"
 # - psutils
 # - subversion
 TERMUX_PKG_VERSION=(5.40.0
-                    388d0eedbfc3864bbbf7ad7f965064d99cac5aaa)
+                    1.5.3)
 TERMUX_PKG_SHA256=(c740348f357396327a9795d3e8323bafd0fe8a5c7835fc1cbaba0cc8dfe7161f
-                   a975c196075623f0dc94f57d00633b0d18ed08e3d85a3ea19d34ece4ec1a94c1)
+                   ecc37b41a60cc3c030413a960cc386455f70c43781c6333d1fcaad02ece32ea8)
 TERMUX_PKG_SRCURL=(http://www.cpan.org/src/5.0/perl-${TERMUX_PKG_VERSION}.tar.gz
-                   https://github.com/arsv/perl-cross/archive/${TERMUX_PKG_VERSION[1]}.tar.gz)
-#                  https://github.com/arsv/perl-cross/releases/download/${TERMUX_PKG_VERSION[1]}/perl-cross-${TERMUX_PKG_VERSION[1]}.tar.gz)
+#                   https://github.com/arsv/perl-cross/archive/${TERMUX_PKG_VERSION[1]}.tar.gz)
+                  https://github.com/arsv/perl-cross/releases/download/${TERMUX_PKG_VERSION[1]}/perl-cross-${TERMUX_PKG_VERSION[1]}.tar.gz)
 TERMUX_PKG_DEPENDS=libandroid-utimes
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_MAKE_PROCESSES=1
